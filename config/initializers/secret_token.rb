4f11c0a36ed02a4ce2989488501f2dd7978b07ff
Jordan Fujimoto <jordanfujimoto@gmail.com>
2014-01-06 15:22:33 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kappaccino::Application.config.secret_key_base = '9995e65bdc13d3d2ab9506d8721c19d4c422ee1804870591f45cfb32d0aec737a2cab168eea39a8ff5d607a3325e3bd2c692fb3bd921abbb182295f90d463093'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kappaccino::Application.config.session_store :cookie_store, key: '_kappaccino_session'
