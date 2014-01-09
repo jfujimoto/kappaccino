b3a4f09ff8cffc3b1bb7c2e31c4ebc40cad0ac91
Jordan Fujimoto <jordanfujimoto@gmail.com>
2014-01-09 15:37:36 -0800

    Add paperclip gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :scrims       
+  has_many :scrims  
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"     
 end
